4bec57abe63d3f18950df26002266026e0a08cc1
chinna <duraichinna197@gmail.com>
2023-06-09 23:43:40 +0530

    just prac

---
+++ remove_nth_node.py
@@ -0,0 +1,17 @@
+class linkedlist:
+    def __init__(self,val)
+        self.val=val
+        self.next=None
+
+class remove:
+    def remove(self,data:int):
+
+        while node==None:
+            if node.val==data:
+                
+
+
+
+                                
+          
+        
